3cfa80683aed73469a2ff96ec309bb26136101e6
illnino ho <illnino.giggs@gmail.com>
2013-03-13 00:46:56 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MzvisaRefinery::Application.config.secret_token = '62940fcf48ec75558f57b1abe6e62b5f41e48d9492242cb7103a2b8650d0699dd2e12a47744da8ee7a89b0829ae4220f7e213ebcf3b5f9ce931df14da0907d35'
